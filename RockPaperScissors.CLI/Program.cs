d8cdf3e334ca9a10b1e66cee0de1b19f57fe97de
Min Chang <minchangmhc@gmail.com>
2021-05-05 13:32:28 -0700

    Add interface logic to get the game running
    Co-authored-by: Jamie Knutsen <knutsenjamie@yahoo.com>
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

---
+++ RockPaperScissors.CLI/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using RockPaperScissors;
+
+public class Program
+{
+  public static void Main()
+  {
+    // RPS gameOne = new RPS();   <-- We don't need this because we are dealing with a static method.
+    Console.WriteLine("Welcome to the game Rock, Paper, Scissors.");
+    Console.Write("Player one please choose Rock, Paper, or Scissors: ");
+    string playerOne = Console.ReadLine();
+    Console.Write("Player two please choose Rock, Paper, or Scissors: ");
+    string playerTwo = Console.ReadLine();
+
+    if (RPS.PlayerOneWin(playerOne, playerTwo) == true)
+    {
+      Console.WriteLine("Player one wins!");
+    }
+    else if (RPS.PlayerTwoWin(playerTwo, playerOne) == true)
+    {
+      Console.WriteLine("Player two wins@!");
+    }
+    else
+    {
+      Console.WriteLine("It was a tie!");
+    }
+  }
+}
